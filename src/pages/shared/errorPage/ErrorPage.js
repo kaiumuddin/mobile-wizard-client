2cd4724d4a411ef9c826bf3b0df329478dffaf46
kaiumuddin <kaium.uddin2909@gmail.com>
2022-11-30 21:13:00 +0600

    Error page modified

--- src/pages/shared/errorPage/ErrorPage.js
+++ src/pages/shared/errorPage/ErrorPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import error from '../../../assets/error.jpg';
 
 const ErrorPage = () => {
     return (
         <div className="hero min-h-screen my-10 bg-base-200">
             <div className="hero-content flex-col">
-                <img alt="" src="https://placeimg.com/400/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
+                <img alt="" src={error} className="w-full rounded-lg shadow-2xl" />
                 <div>
                     <h1 className="text-5xl font-bold">Error!</h1>
                     <p className="py-6">No route found</p>
